32232e537ff3307f3196bf97983570a166fa1a7f
Tomek Marciniak <tmarciniakm@gmail.com>
2023-06-15 14:36:42 +0200

    feat(api): use github hosted json

--- apps/api/src/routers/staking.ts
+++ apps/api/src/routers/staking.ts
@@ -1,8 +1,12 @@
-import validatorsData from '../data/validators.json'
 import { publicProcedure, router } from '../trpc'
 
+const VALIDATORS_URL =
+  'https://raw.githubusercontent.com/mvr-studio/pallad/main/apps/api/src/data/validators.json'
+
 export const stakingRouter = router({
-  index: publicProcedure.query(() => {
-    return validatorsData.validators
+  index: publicProcedure.query(async () => {
+    const request = await fetch(VALIDATORS_URL)
+    const response = await request.json()
+    return response.validators
   })
 })
