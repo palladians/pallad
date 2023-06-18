142eada664198bdc7986fbb3ae3307cefa3ce92d
Tomek Marciniak <tmarciniakm@gmail.com>
2023-06-18 14:33:37 +0200

    chore(website): adjust build settings

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  adapter: netlify()
+  integrations: [tailwind()]
 });
