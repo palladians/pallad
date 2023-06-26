a73535327fa912664ba4179c237b63f71798c81c
Tomek Marciniak <tmarciniakm@gmail.com>
2023-06-26 12:49:02 +0200

    fix(code): fix linter issues

--- packages/features/src/overview/views/Overview.stories.tsx
+++ packages/features/src/overview/views/Overview.stories.tsx
@@ -1,9 +1,9 @@
 import { StoryDefault } from '@ladle/react'
 import { useEffect } from 'react'
 
+import { useAppStore } from '../../common/store/app'
 import { useVaultStore } from '../../common/store/vault'
 import { OverviewView } from './Overview'
-import { useAppStore } from "../../common/store/app";
 
 const WALLET_NAME = 'Demo Wallet'
 const MNEMONIC =
