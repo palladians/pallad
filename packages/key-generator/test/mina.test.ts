a9233e4ce16644016fb883144abb6d91ceb74c36
Teddy <teddyjfpender@gmail.com>
2023-06-27 07:59:13 +0100

    improve mina.test.ts structure

--- packages/key-generator/test/mina.test.ts
+++ packages/key-generator/test/mina.test.ts
@@ -1,57 +1,71 @@
+import { HDKey } from '@scure/bip32'
 import { expect } from 'vitest'
 
 import { MinaKeyGenerator } from '../src/mina'
+import { KeyConst } from '../src/types'
 
-describe('MinaKeyGenerator class', () => {
-  const mina = new MinaKeyGenerator()
-  // currently broken test
-  /*it('reverses bytes', () => {
-    const originalBytes = Buffer.from('abcd', 'hex')
-    const reversedBytes = mina.reverseBytes(originalBytes)
-    console.log(reversedBytes.toString('hex'))
-    expect(reversedBytes.toString('hex')).toEqual('dcba')
-  })*/
+describe('MinaKeyGenerator', () => {
+  let mina: MinaKeyGenerator
+  let mnemonic: string
+  let expectedSeed: string
+  let expectedRootKey: string
 
-  it('gets Hierarchical Deterministic Path', () => {
-    const hdPathObject = {
-      accountIndex: 1,
+  beforeEach(() => {
+    mina = new MinaKeyGenerator()
+    mnemonic =
+      'habit hope tip crystal because grunt nation idea electric witness alert like'
+    expectedSeed =
+      '9d394cc4a658fe8023c6ddf1a6bea918d717543c8eeb30c115051a58993ba26c9af33b2f7f1bae7aa9428933b9e01c026619ac579a9cf789c39b4e10a4bf7240'
+    expectedRootKey =
+      'xprv9s21ZrQH143K3kFcvB64V5doa7swe81fWMQuMVWCbKGq835dB3E5isyDhjimNjhrbRm171Q5D4Uy4wKGixKUvsy53hhaBPHDVenm1tsKzdz'
+  })
+
+  it('should correctly set constants', () => {
+    expect(mina.PURPOSE).to.equal(KeyConst.PURPOSE)
+    expect(mina.COIN_TYPE).to.equal(KeyConst.MINA_COIN_TYPE)
+  })
+
+  it('should return the correct hierarchical deterministic path', () => {
+    const path = mina.getHierarchicalDeterministicPath({
+      accountIndex: 0,
       change: 0,
       index: 0
+    })
+    expect(path).to.equal(
+      `m/${KeyConst.PURPOSE}'/${KeyConst.MINA_COIN_TYPE}'/0'/0/0`
+    )
+  })
+
+  it('should throw an error when unable to derive private key', async () => {
+    // Use a mnemonic that will lead to an error
+    const invalidMnemonic = 'invalid mnemonic'
+    try {
+      await mina.deriveKeyPairByMnemonic(invalidMnemonic, 0, 0, 0)
+      expect.fail('Expected error to be thrown')
+    } catch (error) {
+      if (error instanceof Error) {
+        expect(error.message).to.equal('Invalid mnemonic')
+      } else {
+        expect.fail('Caught exception is not an Error instance')
+      }
     }
-    const hdPath = mina.getHierarchicalDeterministicPath(hdPathObject)
-    expect(hdPath).toEqual("m/44'/12586'/1'/0/0")
   })
 
-  it('derives wallet by mnemonic', async () => {
-    const mnemonic =
-      'habit hope tip crystal because grunt nation idea electric witness alert like'
-    const wallet = await mina.deriveKeyPairByMnemonic(mnemonic, 0)
-
-    expect(wallet).toHaveProperty('privateKey')
-    expect(wallet).toHaveProperty('publicKey')
-    expect(wallet).toHaveProperty('address')
-    expect(wallet.hdIndex).toEqual(0)
+  it('should correctly derive BIP32 Root Key from seed', () => {
+    const seed = Buffer.from(expectedSeed, 'hex')
+    const actualRootKey = HDKey.fromMasterSeed(seed)
+    expect(actualRootKey.privateExtendedKey).to.equal(expectedRootKey)
   })
 
-  it('derives a known keypair', async () => {
-    const mnemonic =
-      'habit hope tip crystal because grunt nation idea electric witness alert like'
+  it('should return the correct wallet data for a known mnemonic', async () => {
+    const expectedWalletData = {
+      privateKey: 'EKExKH31gXH7t5KiYxdyEbtgi22vgX6wnqwmcbrANs9nQJt487iN',
+      publicKey: 'B62qjsV6WQwTeEWrNrRRBP6VaaLvQhwWTnFi4WP4LQjGvpfZEumXzxb',
+      address: 'B62qjsV6WQwTeEWrNrRRBP6VaaLvQhwWTnFi4WP4LQjGvpfZEumXzxb',
+      hdIndex: 0
+    }
+
     const wallet = await mina.deriveKeyPairByMnemonic(mnemonic, 0, 0, 0)
-    console.log(wallet)
-    expect(wallet?.publicKey).toEqual(
-      'B62qjsV6WQwTeEWrNrRRBP6VaaLvQhwWTnFi4WP4LQjGvpfZEumXzxb'
-    )
-    expect(wallet?.privateKey).toEqual(
-      'EKExKH31gXH7t5KiYxdyEbtgi22vgX6wnqwmcbrANs9nQJt487iN'
-    )
-  })
-
-  it('derives different wallets for different account numbers', async () => {
-    const mnemonic =
-      'habit hope tip crystal because grunt nation idea electric witness alert like'
-    const wallet1 = await mina.deriveKeyPairByMnemonic(mnemonic, 0, 0, 0)
-    const wallet2 = await mina.deriveKeyPairByMnemonic(mnemonic, 1, 0, 0)
-
-    expect(wallet1?.publicKey).not.toEqual(wallet2?.publicKey)
+    expect(wallet).to.deep.equal(expectedWalletData)
   })
 })
