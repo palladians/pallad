545a060a2a9c138610d935e31c5350f4535fe1ef
Teddy <teddyjfpender@gmail.com>
2023-06-28 12:54:55 +0100

    Add TSDocs

--- packages/key-generator/src/evm.ts
+++ packages/key-generator/src/evm.ts
@@ -9,10 +9,31 @@ import { HDKey } from '@scure/bip32'
 import { KeyGenerator, KeyPair } from './keyGenerator'
 import { KeyConst } from './types'
 
+/**
+ * EVMKeyGenerator class extends the KeyGenerator.
+ * It is used to generate Ethereum keys, following the Ethereum standards.
+ *
+ * @remarks
+ * This class is part of the {@link @pallad/key-generator#EVMKeyGenerator | EVMKeyGenerator subsystem}.
+ *
+ * @beta
+ */
 export class EVMKeyGenerator extends KeyGenerator {
   PURPOSE = KeyConst.PURPOSE
   COIN_TYPE = KeyConst.ETHEREUM_COIN_TYPE
 
+  /**
+   * Builds a key pair from the provided HD child node and account number.
+   *
+   * @remarks
+   * This method is part of the {@link @pallad/key-generator#EVMKeyGenerator.buildKeyPairFromChildNode | EVMKeyGenerator subsystem}.
+   *
+   * @param childNode - The HDKey child node from which to derive the key pair
+   * @param accountNumber - The account number for the derived key pair
+   * @returns A Promise that resolves to a KeyPair object, containing the derived private key, public key, address, and HD index
+   *
+   * @beta
+   */
   async buildKeyPairFromChildNode(
     childNode: HDKey,
     accountNumber: number
