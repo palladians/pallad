2e7947e743bb2676afd2d51bb655b4b9c6dd8509
Tomek Marciniak <tmarciniakm@gmail.com>
2023-07-16 19:15:48 +0200

    chore(ui): refactor most of the views

---
+++ packages/features/tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    '../ui/**/*.{js,ts,jsx,tsx}',
+    'src/**/*.{js,ts,jsx,tsx}',
+    'ladle/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
