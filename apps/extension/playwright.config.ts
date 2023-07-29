a6a8bc1db2153f207ee14d4260133b1ee0afdd0e
Tomek Marciniak <tmarciniakm@gmail.com>
2023-07-29 09:52:32 +0200

    chore(repo): switch from yarn 1 to pnpm

--- apps/extension/playwright.config.ts
+++ apps/extension/playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
     }
   ],
   webServer: {
-    command: 'yarn preview',
+    command: 'pnpm preview',
     url: BASE_URL,
     reuseExistingServer: !process.env.CI
   }
