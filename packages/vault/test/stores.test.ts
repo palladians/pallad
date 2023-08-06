464942f3985408653b58f75839702b3b7a8d9715
Teddy <teddyjfpender@gmail.com>
2023-08-06 21:40:41 +0100

    fix failing test in vault

--- packages/vault/test/stores.test.ts
+++ packages/vault/test/stores.test.ts
@@ -3,11 +3,12 @@ import { MinaArchiveProvider, MinaProvider } from '@palladxyz/mina-graphql'
 
 import {
   FromBip39MnemonicWordsProps,
+  GroupedCredentials,
   InMemoryKeyAgent,
   MinaPayload,
   MinaSpecificArgs,
   Network
-} from '../../key-management/dist'
+} from '@palladxyz/key-management'
 import { keyAgentStore } from '../src/stores'
 
 // Provide the passphrase for testing purposes
@@ -86,14 +87,14 @@ describe('store', () => {
     // check there exists the first account and address in the keyAgent not in the .serializableKeyAgentData knownCredentials
     const storeKeyAgentCredentials =
       keyAgentStore.getState().keyAgent?.serializableData.credentialSubject
-        .contents
+        .contents as GroupedCredentials[]
     console.log('knownCredentials', storeKeyAgentCredentials)
     expect(storeKeyAgentCredentials).toHaveLength(1)
 
     // check the store has account info
     const accountInformation = keyAgentStore
       .getState()
-      .getAccountStore(storeKeyAgentCredentials[0]?.address)
+      .getAccountStore(network, storeKeyAgentCredentials[0]?.address as string)
     console.log('Account Information!')
     console.log('accountStore', accountInformation)
   })
@@ -168,8 +169,7 @@ describe('store', () => {
       .getState()
       .getAccountStore(
         network,
-        keyAgentStore.getState().keyAgent?.serializableData.credentialSubject
-          .contents[0]?.address
+        keyAgentStore.getState().keyAgent?.serializableData.credentialSubject.contents[0]?.address as string
       )
     console.log('Account Information!')
     console.log('accountStore', devnetAccountInformation)
@@ -183,13 +183,20 @@ describe('store', () => {
       .getState()
       .setCurrentNetwork(newNetwork, providerMainnet, providerArchiveMainnet)
     expect(keyAgentStore.getState().currentNetwork).toEqual(newNetwork)
+    // sync the account information
+    await keyAgentStore.getState().syncAccountStore(
+      keyAgentStore.getState().keyAgent?.serializableData.credentialSubject.contents[0]?.address as string,
+      providerMainnet,
+      providerArchiveMainnet,
+      newNetwork
+    )
     // check the store has account info
     const mainnetAccountInformation = keyAgentStore
       .getState()
       .getAccountStore(
         newNetwork,
         keyAgentStore.getState().keyAgent?.serializableData.credentialSubject
-          .contents[0]?.address
+          .contents[0]?.address as string
       )
     console.log('Account Information!')
     console.log('accountStore', mainnetAccountInformation)
