11215dbc4c704682c05a008d2ff5066ab55da397
Tomek Marciniak <tmarciniakm@gmail.com>
2023-12-14 12:54:33 +0100

    fix(wallet): fix tx fetching

--- packages/features/src/common/hooks/useTransaction.ts
+++ packages/features/src/common/hooks/useTransaction.ts
@@ -6,9 +6,14 @@ import { useAppStore } from '../store/app'
 
 export const useTransaction = ({ hash }: { hash: string }) => {
   const currentWallet = useVault((state) => state.getCurrentWallet())
+  const _syncTransactions = useVault((state) => state._syncTransactions)
   const getTransaction = useVault((state) => state.getTransaction)
   const { publicKey } = currentWallet.accountInfo
   const network = useAppStore((state) => state.network)
+  const syncAndGetTransaction = async () => {
+    await _syncTransactions(network, currentWallet?.credential.credential)
+    return getTransaction(network, publicKey, hash)
+  }
   return useSWR(
     publicKey
       ? [
@@ -17,6 +22,6 @@ export const useTransaction = ({ hash }: { hash: string }) => {
           Mina.Networks[network.toUpperCase() as keyof typeof Mina.Networks]
         ]
       : null,
-    () => getTransaction(network, publicKey, hash)
+    async () => await syncAndGetTransaction()
   )
 }
