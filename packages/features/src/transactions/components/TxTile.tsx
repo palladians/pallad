afe681d50617875c84b34fb647475ebdfb329f47
teddyjfpender <teddyjfpender@gmail.com>
2023-12-20 11:49:42 -0500

    fix(transaction tab): dynamic labelling

--- packages/features/src/transactions/components/TxTile.tsx
+++ packages/features/src/transactions/components/TxTile.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom'
 
-import { StructurizedTransaction } from '../../common/types'
+import { StructurizedTransaction, TxSide } from '../../common/types'
 import { TxIndicator } from './TxIndicator'
 
 interface TxTileProps {
@@ -9,15 +9,23 @@ interface TxTileProps {
 
 export const TxTile = ({ tx }: TxTileProps) => {
   const navigate = useNavigate()
+  const getTransactionLabel = (tx: StructurizedTransaction) => {
+    if (tx.from === tx.to) {
+      return 'Sent to Self'
+    }
+    return tx.side === TxSide.INCOMING ? 'Received' : 'Sent'
+  }
   return (
     <div
       key={tx.hash}
       className="animate-in fade-in flex justify-between items-center gap-4 cursor-pointer"
       onClick={() => navigate(`/transactions/${tx.hash}`)}
     >
-      {tx.kind && <TxIndicator side={tx.side} kind={tx.kind} />}
+      {tx.kind && (
+        <TxIndicator side={tx.side} kind={tx.kind} from={tx.from} to={tx.to} />
+      )}
       <div className="flex flex-col flex-1 gap-2">
-        <div className="flex-1 font-semibold">Received</div>
+        <div className="flex-1 font-semibold">{getTransactionLabel(tx)}</div>
         <div className="flex-1 text-sm">{tx.time}</div>
       </div>
       <div className="font-semibold">{tx.minaAmount} MINA</div>
