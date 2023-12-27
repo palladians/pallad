cca768069c3c75aa8730c9783fb66b5b5fc84ed4
Tomek Marciniak <tmarciniakm@gmail.com>
2023-12-27 22:40:04 +0100

    feat(wallet): add dead simple analytics

--- packages/features/src/settings/views/settings.tsx
+++ packages/features/src/settings/views/settings.tsx
@@ -1,13 +1,16 @@
 import { Mina } from '@palladxyz/mina-core'
 import { useVault } from '@palladxyz/vault'
 import { AlertCircleIcon } from 'lucide-react'
+import { AlertCircleIcon } from 'lucide-react'
 import { useTheme } from 'next-themes'
+import { useState } from 'react'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useSWRConfig } from 'swr'
 
 import { useAppStore } from '@/common/store/app'
 import { AppLayout } from '@/components/app-layout'
+import { RestartWalletAlert } from '@/components/restart-wallet-alert'
 import { RestartWalletAlert } from '@/components/restart-wallet-alert'
 import { Button } from '@/components/ui/button'
 import { Label } from '@/components/ui/label'
@@ -57,7 +60,7 @@ export const SettingsView = () => {
         />
         <div className="flex flex-col gap-4 p-4 flex-1">
           <div className="flex flex-col gap-2">
-            <h2 className="font-semibold">Network</h2>
+            <h2>Network</h2>
             <RadioGroup value={network} onValueChange={handleNetworkSwitch}>
               {/* TODO: Enable after Public Beta <div className="flex items-center space-x-2">
                 <RadioGroupItem
@@ -95,7 +98,7 @@ export const SettingsView = () => {
             </RadioGroup>
           </div>
           <div className="flex flex-col gap-2">
-            <h2 className="font-semibold">Theme</h2>
+            <h2>Theme</h2>
             <RadioGroup value={theme} onValueChange={handleThemeSwitch}>
               <div className="flex items-center space-x-2">
                 <RadioGroupItem value="light" id="themeLight" />
@@ -112,7 +115,7 @@ export const SettingsView = () => {
             </RadioGroup>
           </div>
           <div className="flex flex-col gap-2">
-            <h2 className="font-semibold">Privacy</h2>
+            <h2>Privacy</h2>
             <div className="flex items-center gap-2">
               <Switch
                 checked={shareData}
@@ -122,12 +125,11 @@ export const SettingsView = () => {
             </div>
           </div>
           <div className="flex flex-col gap-2 items-start">
-            <h2 className="font-semibold">Danger Zone</h2>
+            <h2>Danger Zone</h2>
             <Button
               variant="secondary"
               className="gap-2"
               onClick={() => setRestartAlertVisible(true)}
-              data-testid="settings__restartWallet"
             >
               <AlertCircleIcon size={16} />
               Restart Wallet
