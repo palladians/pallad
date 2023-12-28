9d68000a3de5ba3373b7d2a01ea282da54131043
teddyjfpender <teddyjfpender@gmail.com>
2023-12-28 07:59:00 -0500

    fix(remove temp)

--- packages/web-provider-new/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './mina'
-export * from './types'
-export * from './utils'
-export * from './vault-service'
--- packages/web-provider-new/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config' // eslint-disable-line import/no-extraneous-dependencies
-
-export default defineConfig({
-  test: {
-    environment: 'happy-dom',
-    testTimeout: 10000,
-    globals: true
-  }
-})
