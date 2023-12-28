cdab7c79c21f757d40f8f7355d7d6512e5065057
teddyjfpender <teddyjfpender@gmail.com>
2023-12-27 21:54:56 -0500

    chore(lint)

--- packages/mina-graphql/src/Providers/AccountInfo/AccountInfoProvider.ts
+++ packages/mina-graphql/src/Providers/AccountInfo/AccountInfoProvider.ts
@@ -13,19 +13,22 @@ export interface AccountData {
   account: AccountInfo
 }
 
-const customFetch = async (input: RequestInfo | URL, init?: RequestInit): Promise<Response> => {
-  const response = await fetch(input, init);
-  const text = await response.text();
-  const parsed = JSONbig.parse(text);
+const customFetch = async (
+  input: RequestInfo | URL,
+  init?: RequestInit
+): Promise<Response> => {
+  const response = await fetch(input, init)
+  const text = await response.text()
+  const parsed = JSONbig.parse(text)
   return new Response(JSON.stringify(parsed), {
     status: response.status,
     statusText: response.statusText,
     headers: new Headers({
       'Content-Type': 'application/json',
-      ...response.headers,
-    }),
-  });
-};
+      ...response.headers
+    })
+  })
+}
 
 export class AccountInfoGraphQLProvider implements AccountInfoProvider {
   private minaGql: string | null
