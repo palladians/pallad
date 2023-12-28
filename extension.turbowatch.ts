5796629bcfee7a3fb62970bd267fa00d6b6a705b
Tomek Marciniak <tmarciniakm@gmail.com>
2023-12-28 18:31:24 +0100

    chore(wallet): add turbowatch and configure for extension

---
+++ extension.turbowatch.ts
@@ -0,0 +1,19 @@
+import { ChokidarWatcher, defineConfig } from 'turbowatch'
+
+export default defineConfig({
+  project: __dirname,
+  Watcher: ChokidarWatcher,
+  triggers: [
+    {
+      expression: [
+        'allof',
+        ['not', ['dirname', 'node_modules']],
+        ['match', '*.ts', 'basename']
+      ],
+      name: 'build',
+      onChange: async ({ spawn }) => {
+        await spawn`pnpm build:extension`
+      }
+    }
+  ]
+})
