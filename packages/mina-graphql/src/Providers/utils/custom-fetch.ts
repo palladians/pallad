14af5d35f90c655460c061a74de91066f7b4b4d0
teddyjfpender <teddyjfpender@gmail.com>
2023-12-29 19:17:34 -0500

    fix(mina-graphql): revert to JSONbig

--- packages/mina-graphql/src/Providers/utils/custom-fetch.ts
+++ packages/mina-graphql/src/Providers/utils/custom-fetch.ts
@@ -1,27 +1,18 @@
+import JSONbig from 'json-bigint'
+
 export const customFetch = async (
   input: RequestInfo | URL,
   init?: RequestInit
 ): Promise<Response> => {
   const response = await fetch(input, init)
-
-  // Check if the response is OK and the content type is JSON
-  if (
-    response.ok &&
-    response.headers.get('content-type')?.includes('application/json')
-  ) {
-    const json = await response.json()
-    const serialized = JSON.stringify(json)
-    return new Response(serialized, {
-      status: response.status,
-      statusText: response.statusText,
-      headers: new Headers({
-        'Content-Type': 'application/json',
-        ...response.headers
-      })
+  const text = await response.text()
+  const parsed = JSONbig.parse(text)
+  return new Response(JSON.stringify(parsed), {
+    status: response.status,
+    statusText: response.statusText,
+    headers: new Headers({
+      'Content-Type': 'application/json',
+      ...response.headers
     })
-  } else {
-    // If response is not JSON or not OK, return the original response
-    console.log('customFetch non-JSON or error response:', response)
-    return response
-  }
+  })
 }
