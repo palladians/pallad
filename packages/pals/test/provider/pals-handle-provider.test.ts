231383446f94cbc39d0bf101f22a414def0069f9
teddyjfpender <teddyjfpender@gmail.com>
2024-01-24 20:25:16 +0000

    chore: add Pals server docker image to CI

--- packages/pals/test/provider/pals-handle-provider.test.ts
+++ packages/pals/test/provider/pals-handle-provider.test.ts
@@ -1,12 +1,13 @@
 import { createPalsHandleProvider } from '../../src'
+const url = process.env['PALS_SERVER_ENDPOINT'] || ''
 
 describe('createPalsHandleProvider', () => {
   it('should create a pals handle provider', () => {
-    const palsHandleProvider = createPalsHandleProvider('http://127.0.0.1:8000')
+    const palsHandleProvider = createPalsHandleProvider(url)
     expect(palsHandleProvider).toBeDefined()
   })
   it('should get an address from a handle', async () => {
-    const palsHandleProvider = createPalsHandleProvider('http://127.0.0.1:8000')
+    const palsHandleProvider = createPalsHandleProvider(url)
     const result = await palsHandleProvider.getAddressFromHandle({
       handle: '$teddy'
     })
@@ -16,13 +17,13 @@ describe('createPalsHandleProvider', () => {
     )
   })
   it('should check health of Pals Handle Provider', async () => {
-    const palsHandleProvider = createPalsHandleProvider('http://127.0.0.1:8000')
+    const palsHandleProvider = createPalsHandleProvider(url)
     const result = await palsHandleProvider.healthCheck()
     expect(result.ok).toBe(true)
     expect(result.message).toBe('Server is healthy')
   })
   it('should get a list of addresses from a partial handle', async () => {
-    const palsHandleProvider = createPalsHandleProvider('http://127.0.0.1:8000')
+    const palsHandleProvider = createPalsHandleProvider(url)
     const result = await palsHandleProvider.getSearchedHandles({ handle: '$' })
     console.log('Search result', result)
     expect(result.addresses).toHaveLength(5)
