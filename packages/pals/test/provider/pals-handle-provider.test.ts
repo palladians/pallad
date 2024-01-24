6f5b39d6a6851b56e5f182a6b6d65db8bcd3c682
teddyjfpender <teddyjfpender@gmail.com>
2024-01-24 21:04:33 +0000

    fix: remove CI and add msw

--- packages/pals/test/provider/pals-handle-provider.test.ts
+++ packages/pals/test/provider/pals-handle-provider.test.ts
@@ -1,31 +1,36 @@
+import './test-setup'
+
 import { createPalsHandleProvider } from '../../src'
-const url = process.env['PALS_SERVER_ENDPOINT'] || ''
+
+const relativeUrl = ''
 
 describe('createPalsHandleProvider', () => {
   it('should create a pals handle provider', () => {
-    const palsHandleProvider = createPalsHandleProvider(url)
+    const palsHandleProvider = createPalsHandleProvider(relativeUrl)
     expect(palsHandleProvider).toBeDefined()
   })
+
   it('should get an address from a handle', async () => {
-    const palsHandleProvider = createPalsHandleProvider(url)
+    const palsHandleProvider = createPalsHandleProvider(relativeUrl)
     const result = await palsHandleProvider.getAddressFromHandle({
       handle: '$teddy'
     })
-    console.log('Address result', result)
     expect(result.address).toBe(
       'B62qs2mR2g7LB27P36MhxN5jnsnjS8t6azttZfCnAToVpCmTtRVT2nt'
     )
   })
+
   it('should check health of Pals Handle Provider', async () => {
-    const palsHandleProvider = createPalsHandleProvider(url)
+    const palsHandleProvider = createPalsHandleProvider(relativeUrl)
     const result = await palsHandleProvider.healthCheck()
+    console.log('Pals Handle Provider Health Check Response', result)
     expect(result.ok).toBe(true)
     expect(result.message).toBe('Server is healthy')
   })
+
   it('should get a list of addresses from a partial handle', async () => {
-    const palsHandleProvider = createPalsHandleProvider(url)
+    const palsHandleProvider = createPalsHandleProvider(relativeUrl)
     const result = await palsHandleProvider.getSearchedHandles({ handle: '$' })
-    console.log('Search result', result)
     expect(result.addresses).toHaveLength(5)
   })
 })
---
+++ packages/pals/test/provider/test-setup.ts
@@ -0,0 +1,9 @@
+import { setupServer } from 'msw/node'
+
+import { handlers } from './mocks/handlers'
+
+export const server = setupServer(...handlers)
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
