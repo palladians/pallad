baeea9211d8617e3660e4e5d7c3a634adba91384
Mariusz <mariusz.wol45@gmail.com>
2024-04-17 21:34:40 +0100

    chore: add todo test to make it pass

--- packages/pallad-core/test/Mina/custom-token-tx.test.ts
+++ packages/pallad-core/test/Mina/custom-token-tx.test.ts
@@ -157,3 +157,10 @@ describe('Mina InMemoryKeyAgent Signing Custom Token Transaction', () => {
   })
 })
 */
+import { test } from 'vitest' // eslint-disable-line import/no-extraneous-dependencies
+
+test('concatenation', () => {
+  const str1 = 'Hello'
+  const str2 = 'World'
+  expect(str1 + ' ' + str2).toBe('Hello World')
+})
