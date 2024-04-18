3984d417a8b91438f63721ace0623533fd16a313
Mariusz <mariusz.wol45@gmail.com>
2024-04-18 15:53:16 +0100

    feat: add optimism chain-history provider 🚀

--- packages/providers/test/optimism/chain-history/chain-history-provider.test.ts
+++ packages/providers/test/optimism/chain-history/chain-history-provider.test.ts
@@ -1,8 +1,13 @@
 import { Optimism } from '../../../src'
 
 const nodeUrl =
-  process.env['OPTIMISM_ARCHIVE_NODE_URL'] ||
+  process.env['OPTIMISM_NODE_URL'] ||
   'wss://optimism-sepolia-rpc.publicnode.com'
+const archiveNodeurl =
+  process.env['OPTIMISM_ARCHIVE_NODE_URL'] ||
+  'https://api-sepolia-optimism.etherscan.io/' //'https://api.etherscan.io/'
+const publicKey =
+  process.env['PUBLIC_KEY'] || '0xA98005e6ce8E62ADf8f9020fa99888E8f107e3C9'
 const transactionHash =
   process.env['TX_HASH'] ||
   '0x105bf755a57f5f1e70784dd2ce811795e44a443608879e499d459098fb80f560'
@@ -11,7 +16,7 @@ describe('Optimism Chain History Provider (Functional)', () => {
   let provider: ReturnType<typeof Optimism.createChainHistoryProvider>
 
   beforeEach(() => {
-    provider = Optimism.createChainHistoryProvider(nodeUrl)
+    provider = Optimism.createChainHistoryProvider(nodeUrl, archiveNodeurl)
   })
 
   describe('healthCheck', () => {
@@ -22,15 +27,22 @@ describe('Optimism Chain History Provider (Functional)', () => {
     })
   })
 
-  describe('transactions receipt', () => {
+  describe('transactions history', () => {
+    it('should return transaction receipt for a transaction hash', async () => {
+      // This test now depends on the actual response from the server
+      const response = await provider.transactionsByAddresses({
+        addresses: [publicKey]
+      })
+      expect(response[0]).toHaveProperty('blockNumber')
+    })
+  })
+
+  describe.skip('transactions receipt', () => {
     it('should return transaction receipt for a transaction hash', async () => {
       // This test now depends on the actual response from the server
       const response = await provider.transactionsByHashes({
         ids: [transactionHash]
       })
-
-      console.log('Optimism Chain History Provider Response', response)
-
       expect(response).toHaveProperty('blockNumber')
       expect(response).toHaveProperty('blockHash')
     })
