00a5d92037e4101295a2cceae8febbfecab8516b
Mariusz <mariusz.wol45@gmail.com>
2024-04-19 20:52:08 +0100

    chore: add docs to account-info

--- packages/vault/src/account/accountState.ts
+++ packages/vault/src/account/accountState.ts
@@ -9,28 +9,20 @@ export type SingleAccountState = {
 export type ChainAddressMapping = Record<ChainAddress, SingleAccountState>
 
 export type AccountState = {
-  // this should be a record Record<string, ChainAddressMapping> where the string is a network
   accounts: Record<string, ChainAddressMapping>
 }
 
-// TODO: refactor to integrate custom tokens
 export type AccountActions = {
   ensureAccount: (network: string, address: ChainAddress) => void
-  // todo: this should be `setAccountsInfo` because it does multiple accounts
   setAccountInfo: (
-    // ✅ this should be a string
     network: string,
     address: ChainAddress,
-    // ✅ this has to be a record Record<string, AccountInfo> where the string is a ticker of a token
     accountInfo: Record<string, AccountInfo>
   ) => void
 
   setTransactions: (
-    // ✅ this should just be a string
     network: string,
     address: ChainAddress,
-    // ideally this should be a record Record<string, Transaction[]> where the string is a ticker of a token
-    // but for now we only fetch MINA transactions in the chain history provider
     transactions: Record<string, Tx[]>
   ) => void
   getAccountsInfo: (
@@ -40,13 +32,13 @@ export type AccountActions = {
   getAccountInfo: (
     network: string,
     address: ChainAddress,
-    ticker: string // we can add a ticker here to get the account info for a specific token
+    ticker: string
   ) => AccountInfo
 
   getTransactions: (
     network: string,
     address: ChainAddress,
-    ticker: string // we can add a ticker here to get the account info for a specific token
+    ticker: string
   ) => Tx[]
 
   getTransaction: (
