b755dc7f9a203d2422e755547e19ab684817d292
Tomek Marciniak <tmarciniakm@gmail.com>
2024-05-21 21:12:22 +0200

    feat(zeko): add zeko to network switching

--- packages/features/src/wallet/components/tx-tile.tsx
+++ packages/features/src/wallet/components/tx-tile.tsx
@@ -1,5 +1,6 @@
 import { formatCompact } from "@/common/lib/numbers"
 import { TxIcon } from "@/components/tx-icon"
+import { Link } from "react-router-dom"
 
 export const TxTile = ({
   tx,
@@ -10,12 +11,14 @@ export const TxTile = ({
 }) => {
   const formattedAmount = formatCompact({ value: tx.amount })
   return (
-    <div className="card bg-secondary p-4 aspect-square grid-col gap-1">
-      <div className="btn btn-circle bg-neutral">
-        <TxIcon tx={tx} currentWalletAddress={currentWalletAddress} />
+    <Link to={`/transactions/${tx.hash}`}>
+      <div className="card bg-secondary p-4 aspect-square grid-col gap-1">
+        <div className="btn btn-circle bg-neutral">
+          <TxIcon tx={tx} currentWalletAddress={currentWalletAddress} />
+        </div>
+        <h3 className="mt-2">Mina</h3>
+        <p>{formattedAmount}</p>
       </div>
-      <h3 className="mt-2">Mina</h3>
-      <p>{formattedAmount}</p>
-    </div>
+    </Link>
   )
 }
--- packages/features/src/wallet/routes/networks.tsx
+++ packages/features/src/wallet/routes/networks.tsx
@@ -1,7 +1,20 @@
+import { useVault } from "@palladxyz/vault"
 import { useNavigate } from "react-router-dom"
 import { NetworksView } from "../views/networks"
 
 export const NetworksRoute = () => {
   const navigate = useNavigate()
-  return <NetworksView onCloseClicked={() => navigate(-1)} />
+  const switchNetwork = useVault((state) => state.switchNetwork)
+  const _syncWallet = useVault((state) => state._syncWallet)
+  const onNetworkSwitch = async (network: string) => {
+    await switchNetwork(network)
+    await _syncWallet()
+    navigate("/dashboard")
+  }
+  return (
+    <NetworksView
+      onCloseClicked={() => navigate(-1)}
+      onNetworkSwitch={onNetworkSwitch}
+    />
+  )
 }
