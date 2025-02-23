be80195475b3e65d762b3a1ec3fb14b10cf53667
Scott Phillips <36128399+ScottJamesPhillips@users.noreply.github.com>
2025-02-23 08:36:37 +0000

    feat: create WalletList component

---
+++ packages/features/src/wallet-management/components/wallet-list.tsx
@@ -0,0 +1,39 @@
+import { truncateString } from "@/common/lib/string"
+import clsx from "clsx"
+import { type Dispatch, type SetStateAction, useState } from "react"
+import type { Account } from "../types"
+
+type WalletListProps = {
+  accounts: Account[]
+  handleSelect: Dispatch<SetStateAction<Account>>
+}
+
+export const WalletList = ({ accounts, handleSelect }: WalletListProps) => {
+  return (
+    <div className="space-y-2 py-5">
+      <div className="text-mint">Other</div>
+      <div className="text-2xl">Wallets</div>
+      {accounts.map((account) => {
+        return (
+          <button
+            key={account.name}
+            type="button"
+            className={clsx(
+              "bg-secondary w-full px-6 py-4 flex justify-between rounded-2xl hover:bg-primary",
+            )}
+            onClick={() => handleSelect(account)}
+          >
+            <p>{account.name}</p>
+            <p>
+              {truncateString({
+                value: account.publicKey,
+                endCharCount: 3,
+                firstCharCount: 5,
+              })}
+            </p>
+          </button>
+        )
+      })}
+    </div>
+  )
+}
