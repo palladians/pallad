01f364c7ca28ace2d9d34ead9983563222cc99df
Scott Phillips <36128399+ScottJamesPhillips@users.noreply.github.com>
2025-03-10 13:41:33 +0000

    refactor: DropdownOption onClick prop type of SingleCredState

--- packages/features/src/account-management/types.ts
+++ packages/features/src/account-management/types.ts
@@ -1,5 +1,7 @@
+import type { SingleCredentialState } from "@palladxyz/vault"
+
 export type DropdownOption = {
   name: string
   icon: JSX.Element
-  onClick: (pubKey: string) => void
+  onClick: (account: SingleCredentialState) => void
 }
